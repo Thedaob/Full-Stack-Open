3eb3427552cd5052d0ddd370313fcf4d2a705fa7
Daniel Ortega Bello <53585546+Thedaob@users.noreply.github.com>
2024-05-17 22:56:29 -0500

    Se modifica segun lo establecido en la clase 2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,21 +1,24 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0)
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total totalExercises={totalExercises} />
-    </div>
+    <Header course={course} />
+    <Content parts={parts} />
+    <Total parts={parts} />
+  </div>
   )
 }
-
 export default App
